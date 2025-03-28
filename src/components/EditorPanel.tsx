da2c55ec00cd176bf74d34e1ae8c2563d03b505f
artemkv <artemkv@gmail.com>
2025-03-28 23:42:31 +0100

    use MDXEditor to render md

--- src/components/EditorPanel.tsx
+++ src/components/EditorPanel.tsx
@@ -1,10 +1,32 @@
-import { Note, NoteState } from "../model";
 import "./EditorPanel.css";
+import "@mdxeditor/editor/style.css";
+import "github-markdown-css";
+import { Note, NoteState } from "../model";
 import Empty from "./Empty";
 import ProgressIndicator from "./ProgressIndicator";
+import {
+  headingsPlugin,
+  listsPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  MDXEditorMethods,
+  quotePlugin,
+  thematicBreakPlugin,
+} from "@mdxeditor/editor";
+import { useEffect, useRef } from "react";
 
 function EditorPanel(props: { note: Note | undefined }) {
   const note = props.note;
+
+  // TODO: maybe integrate this better with the rest of the ui logic
+  const ref = useRef<MDXEditorMethods>(null);
+  useEffect(() => {
+    let noteText = "";
+    if (note?.state == NoteState.Loaded) {
+      noteText = note.text;
+    }
+    ref.current?.setMarkdown(noteText);
+  }, [note]);
 
   if (note == undefined) {
     return (
@@ -33,7 +55,20 @@ function EditorPanel(props: { note: Note | undefined }) {
     return (
       <div className="editor-panel">
         <div className="editor-panel-left" />
-        <div className="editor-panel-inner">{note.text}</div>
+        <div className="editor-panel-inner">
+          <MDXEditor
+            contentEditableClassName="markdown-body"
+            ref={ref}
+            markdown=""
+            plugins={[
+              headingsPlugin(),
+              listsPlugin(),
+              quotePlugin(),
+              thematicBreakPlugin(),
+              markdownShortcutPlugin(),
+            ]}
+          />
+        </div>
         <div className="editor-panel-right" />
       </div>
     );
